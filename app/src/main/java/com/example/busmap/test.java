29e582ab6ec823215021c9bf76cabf27a830171e
Trungtien93 <kasadin2k@gmail.com>
2024-10-07 09:35:10 +0700

    add method
    method onCreate

--- app/src/main/java/com/example/busmap/test.java
+++ app/src/main/java/com/example/busmap/test.java
@@ -1,4 +1,13 @@
 package com.example.busmap;
 
-public class test {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class test extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
+
