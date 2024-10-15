330e070311ee868d19b158da799a2e58918c07d7
nmt1511 <nmthuan175@gmail.com>
2024-10-15 11:06:54 +0700

    sua layout

--- app/src/main/java/com/example/busmap/Login.java
+++ app/src/main/java/com/example/busmap/Login.java
@@ -1,5 +1,32 @@
 package com.example.busmap;
 
-public class Login {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+public class Login extends AppCompatActivity  {
+    EditText username;
+    EditText password;
+    Button loginButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Toast.makeText(Login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Login.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 }
