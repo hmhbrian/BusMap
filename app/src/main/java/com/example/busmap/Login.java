d1e9ba6198920a03b36c301c0dbc0c8579a38298
Trungtien93 <kasadin2k@gmail.com>
2024-10-15 11:44:12 +0700

    text

--- app/src/main/java/com/example/busmap/Login.java
+++ app/src/main/java/com/example/busmap/Login.java
@@ -1,13 +1,15 @@
 package com.example.busmap;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class Login extends AppCompatActivity  {
+public class Login extends Activity {
     EditText username;
     EditText password;
     Button loginButton;
