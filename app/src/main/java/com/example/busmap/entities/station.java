ed1f595958f0253da2a5790232adb8432a07503a
brianmhoang <165451083+brianmhoang@users.noreply.github.com>
2025-01-06 15:54:22 +0700

    Hoang does the destination station search function

---
+++ app/src/main/java/com/example/busmap/entities/station.java
@@ -0,0 +1,49 @@
+package com.example.busmap.entities;
+
+import java.io.Serializable;
+
+public class station implements Serializable {
+    private int id;
+    private String name;
+    private double latitude;
+    private double longitude;
+
+    public station(int id, String name, double latitude, double longitude) {
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
